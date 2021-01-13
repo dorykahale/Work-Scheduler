0f508ab6f396f94c1d48b1c45ee8edf4c9a471e2
DMC <dorykahale@gmail.com>
2021-01-12 20:37:35 -0600

    restructured folder hierarchy

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,18 @@
 $(document).ready(function () {
 
+    //display current day & time
     $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
-
+    // save button event listener 
     $(".saveBtn").on("click", function () {
    
         console.log(this);
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
 
+        //sets local storage
         localStorage.setItem(time, text);
     })
-
+    //loads saved LocalStorage data for every hour
     $("#hour8 .description").val(localStorage.getItem("hour8"));
     $("#hour9 .description").val(localStorage.getItem("hour9"));
     $("#hour10 .description").val(localStorage.getItem("hour10"));
@@ -22,15 +24,21 @@ $(document).ready(function () {
     $("#hour16 .description").val(localStorage.getItem("hour16"));
     $("#hour17 .description").val(localStorage.getItem("hour17"));
     $("#hour18 .description").val(localStorage.getItem("hour18"));
+    $("#hour19 .description").val(localStorage.getItem("hour19"));
+    $("#hour20 .description").val(localStorage.getItem("hour20"));
+    $("#hour21 .description").val(localStorage.getItem("hour21"));
 
-    function hourTrack() {
 
+    function hourTrack()  {
+        //get current hour
         var currentHour = moment().hour();
 
+        // loop over hourblocks
         $(".time-block").each(function () {
             var hourBlock = parseInt($(this).attr("id").split("hour")[1]);
             console.log( hourBlock, currentHour)
 
+            //establish current hourblock  
             if (hourBlock < currentHour) {
                 $(this).addClass("past");
                 $(this).removeClass("future");
